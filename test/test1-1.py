108d85d84eca97cd6bf53b392d99154d78d570d4
bako98 <kcjgh5132@gmail.com>
2025-02-14 15:38:24 +0900

    a

--- test/test1-1.py
+++ test/test1-1.py
@@ -1,1 +1,10 @@
 print("world hellow")
+
+class Man:
+    def __init__(self,name,power):
+        self.name = name
+        self.power = power
+    def info(self):
+        print(self.name,self.power)
+
+Man("Bob",90).info()
